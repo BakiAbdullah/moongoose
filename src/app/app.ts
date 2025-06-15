5ebb6c5c4427e46cd6504a846ce8f36dc18a8611
BakiAbdullah <shatil605@gmail.com>
2025-06-16 01:56:24 +0600

    mongoose schema added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Todos App!");
+const noteSchema = new Schema({
+  title: String,
+  content: String,
+  publishDate: Number
 });
 
+// model name and schema
+const Note = model("Note", noteSchema);
+
+app.post("/create-note", async (req: Request, res: Response) => {
+  const myNote = new Note({
+    title: "Learning Mongoose",
+    content: "I am learning mongoose",
+    publishDate: "hello"
+  })
+
+  await myNote.save();
+  
+  res.status(201).json({
+    success: true,
+    message: "Note created successfully.",
+    note: myNote
+  })
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Note App!");
+});
 
 export default app;
-
-
--- src/app/server.ts
+++ src/app/server.ts
@@ -7,14 +7,16 @@ const PORT = 5000;
 
 async function main() {
   try {
+    await mongoose.connect(
+      "mongodb+srv://bakiabdullah:todoapp@cluster0.uqgxsrk.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("Connected to MongoDb with Mongoose!");
     server = app.listen(PORT, () => {
-      console.log(`App is listening on port ${PORT}`)
-    })
+      console.log(`App is listening on port ${PORT}`);
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
-main()
-
-// mongodb+srv://bakiabdullah:bakiabdullah@cluster0.uqgxsrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+main();
